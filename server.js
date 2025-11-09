b8b966875d0349cdf5ffc9676d0f9a6ea72c0241
unknown <you@example.com>
2025-11-09 19:40:33 +0300

    mpesa

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import Pesarouter from './Routes/Pesaroutes.js'
 const app=express()
 const PORT = process.env.PORT || 4000
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
 	origin:["http://localhost:5173","https://mygold-two.vercel.app"],

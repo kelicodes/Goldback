55d42b514c29d3a1121a89e7dd27cac257142796
unknown <you@example.com>
2025-11-09 14:17:31 +0300

    back

--- Controllers/Cartcontroller.js
+++ Controllers/Cartcontroller.js
@@ -61,6 +61,7 @@ export const removeFromCart = async (req, res) => {
 
     res.status(200).json(cart);
   } catch (error) {
+    console.log(error)
     res.status(500).json({ message: "Server error", error });
   }
 };
